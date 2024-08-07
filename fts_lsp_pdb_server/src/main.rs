588a74656923a1ad70a401fea49125ad04c30c24
Forrest Smith <forrestthewoods@gmail.com>
2024-08-07 01:41:00 -0700

    it works with multiple PDBs!

--- fts_lsp_pdb_server/src/main.rs
+++ fts_lsp_pdb_server/src/main.rs
@@ -8,7 +8,6 @@ use normpath::PathExt;
 use pdb::{FallibleIterator, PDB};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 use std::pin::Pin;
 use std::str::FromStr;
@@ -239,7 +238,7 @@ async fn handle_request(req: Request, connection: &Connection, cache: &Cache) {
 }
 
 async fn goto_definition(params: GotoDefinitionParams, cache: &Cache) -> anyhow::Result<GotoDefinitionResponse> {
-    let mut debug_log = std::fs::OpenOptions::new().create(true).append(true).open("c:/temp/hack_log.txt")?;
+    //let mut debug_log = std::fs::OpenOptions::new().create(true).append(true).open("c:/temp/hack_log.txt")?;
 
     // Input data
     let uri = params.text_document_position_params.text_document.uri;
@@ -247,101 +246,113 @@ async fn goto_definition(params: GotoDefinitionParams, cache: &Cache) -> anyhow:
     let source_file = filepath.normalize()?.as_path().to_string_lossy().to_lowercase();
     let line_number = params.text_document_position_params.position.line + 1;
 
-    debug_log.write_all("Target File: ".as_bytes())?;
-    debug_log.write_all(source_file.as_bytes())?;
-    debug_log.write_all("\n".as_bytes())?;
+    // debug_log.write_all("Target File: ".as_bytes())?;
+    // debug_log.write_all(source_file.as_bytes())?;
+    // debug_log.write_all("\n".as_bytes())?;
 
-    // Reparse PDB because Rust is stupid
-    let pdb_file = File::open(&cache.borrow_owner().pdb_path)?;
-    let mut pdb = PDB::open(pdb_file)?;
-    let address_map = pdb.address_map()?;
+    for exe_cache in &cache.exe_caches {
+        let result = || -> anyhow::Result<GotoDefinitionResponse> {
+            // Reparse PDB because Rust is stupid
+            let pdb_file = File::open(&exe_cache.borrow_owner().pdb_path)?;
+            let mut pdb = PDB::open(pdb_file)?;
+            let address_map = pdb.address_map()?;
 
-    // Find exe address range for (source_file, line_info)
-    let (start, end) = if let Some(line) = cache.borrow_owner().files.get(&source_file).and_then(|lines| lines.get(&line_number)) {
-        let rva = line
-            .offset
-            .to_rva(&address_map)
-            .ok_or_else(|| anyhow!("Could not map line offset to RVA"))?;
-        (rva.0, rva.0 + line.length.unwrap_or_default())
-    } else {
-        anyhow::bail!("Could not fine entry for [{source_file}]:[{line_number}]");
-    };
+            // Find exe address range for (source_file, line_info)
+            let owner = exe_cache.borrow_owner();
+            let files = &owner.files;
+            let (start, end) = if let Some(line) = files.get(&source_file).and_then(|lines| lines.get(&line_number)) {
+                let rva = line
+                    .offset
+                    .to_rva(&address_map)
+                    .ok_or_else(|| anyhow!("Could not map line offset to RVA"))?;
+                (rva.0, rva.0 + line.length.unwrap_or_default())
+            } else {
+                anyhow::bail!("Could not fine entry for [{source_file}]:[{line_number}]");
+            };
 
-    // Find calls made within line
-    let mut source_locations: Vec<(String, u32)> = Default::default();
-    let pe = &cache.borrow_dependent().borrow_owner().exe_parsed;
-    let section = pe
-        .sections
-        .iter()
-        .find(|sec| sec.virtual_address <= start && start < sec.virtual_address + sec.virtual_size);
-    if let Some(section) = section {
-        let offset = (start - section.virtual_address) as usize;
-        let size = (end - start) as usize;
-        let bytes =
-            &cache.borrow_owner().exe_bytes[section.pointer_to_raw_data as usize + offset..section.pointer_to_raw_data as usize + offset + size];
-        let instructions = cache.borrow_owner().exe_capstone.disasm_all(bytes, start as u64)?;
+            // Find calls made within line
+            let mut source_locations: Vec<(String, u32)> = Default::default();
+            let pe = &exe_cache.borrow_dependent().borrow_owner().exe_parsed;
+            let section = pe
+                .sections
+                .iter()
+                .find(|sec| sec.virtual_address <= start && start < sec.virtual_address + sec.virtual_size);
+            if let Some(section) = section {
+                let offset = (start - section.virtual_address) as usize;
+                let size = (end - start) as usize;
+                let bytes =
+                    &exe_cache.borrow_owner().exe_bytes[section.pointer_to_raw_data as usize + offset..section.pointer_to_raw_data as usize + offset + size];
+                let instructions = exe_cache.borrow_owner().exe_capstone.disasm_all(bytes, start as u64)?;
 
-        for instruction in instructions.iter() {
-            //println!("{}", instruction);
-            if instruction.mnemonic().unwrap_or_default() == "call" {
-                // looks like op_code performs the relative address computation for us
-                // maybe_target_address == target_address
-                let offset_bytes = &instruction.bytes()[1..5];
-                let offset = i32::from_le_bytes([offset_bytes[0], offset_bytes[1], offset_bytes[2], offset_bytes[3]]);
-                let _maybe_target_address = (instruction.address() as i64 + instruction.len() as i64) + (offset as i64);
+                for instruction in instructions.iter() {
+                    //println!("{}", instruction);
+                    if instruction.mnemonic().unwrap_or_default() == "call" {
+                        // looks like op_code performs the relative address computation for us
+                        // maybe_target_address == target_address
+                        let offset_bytes = &instruction.bytes()[1..5];
+                        let offset = i32::from_le_bytes([offset_bytes[0], offset_bytes[1], offset_bytes[2], offset_bytes[3]]);
+                        let _maybe_target_address = (instruction.address() as i64 + instruction.len() as i64) + (offset as i64);
 
-                let op_str = instruction.op_str().unwrap();
-                let mut target_address = u64::from_str_radix(op_str.trim_start_matches("0x"), 16).unwrap();
-
-                // lookup target instruction, may be jmp
-                let maybe_inst = cache.borrow_dependent().borrow_dependent().exe_instructions_sorted.get(&target_address);
-                if let Some(instruction) = maybe_inst {
-                    if instruction.mnemonic().unwrap_or_default() == "jmp" {
                         let op_str = instruction.op_str().unwrap();
-                        let new_target_address = u64::from_str_radix(op_str.trim_start_matches("0x"), 16).unwrap();
-                        //println!("    Remapping call 0x{:x} to 0x{:x}", target_address, new_target_address);
-                        target_address = new_target_address;
+                        let mut target_address = u64::from_str_radix(op_str.trim_start_matches("0x"), 16).unwrap();
+
+                        // lookup target instruction, may be jmp
+                        let maybe_inst = exe_cache.borrow_dependent().borrow_dependent().exe_instructions_sorted.get(&target_address);
+                        if let Some(instruction) = maybe_inst {
+                            if instruction.mnemonic().unwrap_or_default() == "jmp" {
+                                let op_str = instruction.op_str().unwrap();
+                                let new_target_address = u64::from_str_radix(op_str.trim_start_matches("0x"), 16).unwrap();
+                                //println!("    Remapping call 0x{:x} to 0x{:x}", target_address, new_target_address);
+                                target_address = new_target_address;
+                            }
+                        }
+
+                        let symcache = &exe_cache.borrow_dependent().borrow_owner().symcache;
+                        let m = symcache.lookup(target_address).collect::<Vec<_>>();
+                        if m.len() == 0 {
+                            anyhow::bail!("Could not find function at address [0x{:x}]", target_address);
+                        }
+
+                        let source_loc = &m[0];
+                        let path = source_loc.file().map(|file| file.full_path()).unwrap_or_else(|| "<unknown file>".into());
+                        let line = source_loc.line();
+                        source_locations.push((path, line));
                     }
                 }
-
-                let symcache = &cache.borrow_dependent().borrow_owner().symcache;
-                let m = symcache.lookup(target_address).collect::<Vec<_>>();
-                if m.len() == 0 {
-                    anyhow::bail!("Could not find function at address [0x{:x}]", target_address);
-                }
-
-                let source_loc = &m[0];
-                let path = source_loc.file().map(|file| file.full_path()).unwrap_or_else(|| "<unknown file>".into());
-                let line = source_loc.line();
-                source_locations.push((path, line));
             }
+
+            source_locations = source_locations.into_iter().unique().collect();
+
+            if source_locations.len() == 0 {
+                anyhow::bail!("Failed to map file/line to anything");
+            }
+
+            let lsp_locations: Vec<_> = source_locations
+                .iter()
+                .filter_map(|sl| {
+                    let file = &sl.0;
+                    let line = sl.1;
+                    let pos = Position::new(line - 1, 0); // lines start at 0?
+                    url::Url::from_file_path(&file).ok().and_then(|uri| {
+                        Some(lsp_types::Location {
+                            uri: uri,
+                            range: lsp_types::Range { start: pos, end: pos },
+                        })
+                    })
+                })
+                .collect();
+
+            if lsp_locations.len() == 1 {
+                return Ok(GotoDefinitionResponse::Scalar(lsp_locations[0].clone()));
+            } else {
+                return Ok(GotoDefinitionResponse::Array(lsp_locations));
+            }
+        }();
+
+        if result.is_ok() {
+            return result;
         }
     }
 
-    source_locations = source_locations.into_iter().unique().collect();
-
-    if source_locations.len() == 0 {
-        anyhow::bail!("Failed to map file/line to anything");
-    }
-
-    let lsp_locations: Vec<_> = source_locations
-        .iter()
-        .filter_map(|sl| {
-            let file = &sl.0;
-            let line = sl.1;
-            let pos = Position::new(line - 1, 0); // lines start at 0?
-            url::Url::from_file_path(&file).ok().and_then(|uri| {
-                Some(lsp_types::Location {
-                    uri: uri,
-                    range: lsp_types::Range { start: pos, end: pos },
-                })
-            })
-        })
-        .collect();
-
-    if lsp_locations.len() == 1 {
-        return Ok(GotoDefinitionResponse::Scalar(lsp_locations[0].clone()));
-    } else {
-        return Ok(GotoDefinitionResponse::Array(lsp_locations));
-    }
+    anyhow::bail!("Failed to find [{:?}]:[{}] in any PDB", &filepath, line_number);
 }
